10d84393af859e3d2bc4c93877215eb9c110bd88
koduki <pascalm3+git@gmail.com>
2019-09-29 00:55:58 -0700

    Remove debug code

--- deploy.sh
+++ deploy.sh
@@ -13,8 +13,8 @@ rm -rf ${GIT_ID}.github.io
 git clone ${REMOTE_REPOS}
 
 echo "Deploy ${SRC_DIR} to ${DEPLOY_DIR}"
-ls -l 
 rm -rf ${GIT_ID}.github.io/${DEPLOY_DIR}
+mkdir -p ${GIT_ID}.github.io/${DEPLOY_DIR}
 mv ${SRC_DIR} ${GIT_ID}.github.io/${DEPLOY_DIR}
 
 echo "Push"
